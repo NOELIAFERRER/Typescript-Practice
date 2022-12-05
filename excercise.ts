ab97684358fd85619ba2ca7fb5a0baf7f4f39207
Noelia Ferrer <noelia.c.ferrer@gmail.com>
2022-12-04 22:21:52 -0300

    trabajando

---
+++ excercise.ts
@@ -0,0 +1,24 @@
+let myTypeString: string = 'Hello darling';
+let myTypeNumber: number = 46;
+let myTypeBoolean: boolean = true;
+
+let arrNumber: number[] = [1, 2, 3];
+let arrNumber2: Array<number> = [1, 2, 3];
+let arrStrings: string[] = ['Uno', 'dos', 2, true]; //solo permite arrays
+
+let arrAny: any[] = ['uno', 2, true];
+//tuple
+let tuplePlayers: [string, number, boolean] = ['Suzie', 2, true];
+
+//tuple Array
+let players: [number, string][];
+players = [
+    [1, 'uno'],
+    [2, 'dos'],
+    [3, 'tres']
+];
+
+let myVariable;
+
+let myVariable1: string;
+let
